3020c71dcb3d3bedeb572e60616518e4298662bf
Carlos Alejandro Avilez Jimenez <carlos.avilez@softtekclients.com>
2020-09-09 15:08:43 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TextField from "./TextField";
+import Counter from "./Counter";
+import Display from "./Display";
 
 function App() {
+  const [counter, setCounter] = useState(50);
+  const handleSubmit = () => setCounter(counter + 1);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Counter handleSubmit={handleSubmit} counter={counter} />
+        <Display message={counter} />
       </header>
     </div>
   );
---
+++ src/Counter.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+interface props {
+  handleSubmit: () => void;
+  counter: number;
+}
+const Counter: React.FC<props> = (props) => {
+  return (
+    <div>
+      <button type="submit" onClick={props.handleSubmit}>
+        {props.counter}
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
